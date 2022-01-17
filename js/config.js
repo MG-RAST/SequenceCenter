e15494fb916d28e27a4059f8ba67a7b61ac40ec8
Andreas Wilke <wilke@anl.gov>
2022-01-17 11:42:17 -0600

    Changed cgi path and shock preauth url

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var RetinaConfig = {
     // server urls
     "shock_url": "https://sequencing.bio.anl.gov",
-    "shock_preauth": "http://shock.metagenomics.anl.gov/preauth",
+    "shock_preauth": "http://shock.mg-rast.org/preauth",
     "auth_url": "https://sequencing.bio.anl.gov/cgi-bin/oAuth.cgi"
 };
